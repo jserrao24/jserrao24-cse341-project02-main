6e2333d398878a90fd3e736e3fdd24b64f8e7f91
Jared Serrao <jared.serrao5@gmail.com>
2024-01-31 10:00:01 -1000

    test

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
   //#swagger.tags = ['Artists']
   try {
-    const result = await mongodb.getDatabase().db().collection('artists').find();
+    const result = await mongodb.getDatabase().db("artists").collection('artists').find();
     result.toArray().then((artists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(artists);
